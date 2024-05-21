e105cdc62f2caddb2d575c002e2b1f2b7456743d
andronedev <andronedev@gmail.com>
2024-05-21 16:32:35 +0200

    add transit patches

---
+++ src/main/kotlin/app/revanced/patches/transit/misc/TransitUnlockPatch.kt
@@ -0,0 +1,22 @@
+package app.revanced.patches.transit.misc
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.transit.misc.fingerprints.IsPremiumFingerprint
+
+@Patch(
+    name = "Transit",
+    description = "Unlock All Pro features in Transit",
+    compatiblePackages = [
+        CompatiblePackage("com.thetransitapp.droid", ["5.15.16"]),
+    ],
+)
+@Suppress("unused")
+object TransitUnlockPatch :  BytecodePatch(setOf(IsPremiumFingerprint)) {
+    override fun execute(context: BytecodeContext) = IsPremiumFingerprint.result?.let { result ->
+        val isSubscribedIndex = result.scanResult.patternScanResult!!.startIndex
+        result.mutableMethod.replaceInstruction(isSubscribedIndex, "const/4 v0, 0x1")
+    } ?: throw IllegalStateException("Fingerprint not found")
+}
