a74d108570f21447b73dfdf016a9b337b7ae2f08
andronedev <andronedev@gmail.com>
2024-05-21 18:11:19 +0200

    fix(pencil): fix unlock patch

--- src/main/kotlin/app/revanced/patches/transit/misc/fingerprints/IsPremiumFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/transit/misc/fingerprints/IsPremiumFingerprint.kt
@@ -4,8 +4,8 @@ import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
-
 /*
+
 .method public static fetchSubscriptionStatus(ZJ)V
     .locals 20
 
@@ -180,8 +180,8 @@ import com.android.tools.smali.dexlib2.Opcode
  */
 internal object IsPremiumFingerprint : MethodFingerprint(
     // this methode is used to find the fingerprint of the method fetchSubscriptionStatus
+    returnType = "V",
     accessFlags = AccessFlags.PUBLIC or AccessFlags.STATIC,
-    customFingerprint = { methodDef, _ ->
-        methodDef.name == "fetchSubscriptionStatus"
-    },
+    strings = listOf("ForceHasActiveRoyaleSubscription", "product1234"),
+
 )
