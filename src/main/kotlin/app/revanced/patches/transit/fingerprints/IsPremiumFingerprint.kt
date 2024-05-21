704682f7a387e5340b6a62359b6e0b9be85f5bed
andronedev <andronedev@gmail.com>
2024-05-21 14:18:21 +0200

    wip

--- src/main/kotlin/app/revanced/patches/transit/fingerprints/IsPremiumFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/transit/fingerprints/IsPremiumFingerprint.kt
@@ -5,12 +5,6 @@ import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-/*
-    .line 11
-    .line 12
-    invoke-static {v2}, Lcom/thetransitapp/droid/shared/data/TransitLib;->getEnvironmentVariableBooleanValue(Ljava/lang/String;)Z
-    move-result v2
- */
 internal object IsPremiumFingerprint : MethodFingerprint(
     customFingerprint ={ methodDef, classDef ->
         classDef.type.endsWith("TransitLib") && methodDef.name == "getEnvironmentVariableBooleanValue"
