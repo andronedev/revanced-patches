d43d4a8c240523a82891ac4fccbb167bacc44df2
andronedev <andronedev@gmail.com>
2024-05-22 10:55:57 +0200

    fix(transit): fix patch

--- src/main/kotlin/app/revanced/patches/transit/misc/TransitUnlockPatch.kt
+++ src/main/kotlin/app/revanced/patches/transit/misc/TransitUnlockPatch.kt
@@ -4,7 +4,7 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.transit.misc.fingerprints.getMaxAlertsFingerprint
+import app.revanced.patches.transit.misc.fingerprints.IsPremiumFingerprint
 
 @Patch(
     name = "Pro Features Unlock",
@@ -14,8 +14,8 @@ import app.revanced.patches.transit.misc.fingerprints.getMaxAlertsFingerprint
     ],
 )
 @Suppress("unused")
-object TransitUnlockPatch :  BytecodePatch(setOf(getMaxAlertsFingerprint)) {
-    override fun execute(context: BytecodeContext) = getMaxAlertsFingerprint.result?.let { result ->
+object TransitUnlockPatch :  BytecodePatch(setOf(IsPremiumFingerprint)) {
+    override fun execute(context: BytecodeContext) = IsPremiumFingerprint.result?.let { result ->
         // put v2 var with true
         result.mutableMethod.replaceInstruction(6, "const/4 v2, 0x1")
     } ?: throw IllegalStateException("Fingerprint not found")
